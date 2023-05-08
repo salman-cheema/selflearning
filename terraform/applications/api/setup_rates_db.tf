04dde26ef79c64636e7f79190fa326ae8a4c9cc5
Muhammad  Suleman <muhammad.suleman@ip-192-168-10-3.ec2.internal>
2023-05-09 02:59:01 +0500

    first commit

--- terraform/applications/api/setup_rates_db.tf
+++ terraform/applications/api/setup_rates_db.tf
@@ -31,8 +31,7 @@ resource "random_password" "rates_rw_password" {
 # PRE REQ
 # psql-client needs to be installled at the machine from where the terraform is running
 resource "null_resource" "setup_db" {
-    depends_on = [ module.rds,
-   postgresql_database.rates ] #wait for the rds to be ready
+    depends_on = [ module.rds] #wait for the rds to be ready
   provisioner "local-exec" {
     command     = <<EOT
     psql 'user=${local.rates_api_username} password=${module.rates_rds_password.value} host=${module.rds.rds_writer_internal_endpoint} dbname=${local.rates_api_db_name}' -c "create database ${local.rates_api_db_name}"
