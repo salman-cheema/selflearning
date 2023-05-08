239e42ab080226be2b15ecf71dc8f21f18fc48e9
Muhammad  Suleman <muhammad.suleman@ip-192-168-10-3.ec2.internal>
2023-05-09 02:58:26 +0500

    first commit

--- terraform/applications/api/setup_rates_db.tf
+++ terraform/applications/api/setup_rates_db.tf
@@ -2,11 +2,11 @@ locals {
   rates_api_db_name = "rates"
   rates_api_username = "rates_rw"
 }
-resource "postgresql_database" "rates" {
-  name              = local.rates_api_db_name
-  depends_on = [ module.rds,
-   aws_route53_zone.internal_selflearning]
-}
+# resource "postgresql_database" "rates" {
+#   name              = local.rates_api_db_name
+#   depends_on = [ module.rds,
+#    aws_route53_zone.internal_selflearning]
+# }
 
 resource "random_password" "rates_rw_password" {
   length           = 16
@@ -21,12 +21,12 @@ resource "random_password" "rates_rw_password" {
   depends_on = [ module.rds ]
 }
 
-resource "postgresql_role" "rates_rw_role" {
-  name     = local.rates_api_username
-  login    = true
-  password = random_password.rates_rw_password.result
-  depends_on = [ module.rds ]
-}
+# resource "postgresql_role" "rates_rw_role" {
+#   name     = local.rates_api_username
+#   login    = true
+#   password = random_password.rates_rw_password.result
+#   depends_on = [ module.rds ]
+# }
 
 # PRE REQ
 # psql-client needs to be installled at the machine from where the terraform is running
@@ -34,7 +34,11 @@ resource "null_resource" "setup_db" {
     depends_on = [ module.rds,
    postgresql_database.rates ] #wait for the rds to be ready
   provisioner "local-exec" {
-    command = "psql 'user=${local.rates_api_username} password=${module.rates_rds_password.value} host=${module.rds.rds_writer_internal_endpoint} dbname=${local.rates_api_db_name}' < files/db/rates.sql"
+    command     = <<EOT
+    psql 'user=${local.rates_api_username} password=${module.rates_rds_password.value} host=${module.rds.rds_writer_internal_endpoint} dbname=${local.rates_api_db_name}' -c "create database ${local.rates_api_db_name}"
+    psql 'user=${local.rates_api_username} password=${module.rates_rds_password.value} host=${module.rds.rds_writer_internal_endpoint} dbname=${local.rates_api_db_name}' < files/db/rates.sql
+    EOT
+    interpreter = ["/bin/bash", "-c"]
   }
 }
 
