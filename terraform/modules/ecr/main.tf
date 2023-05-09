89aff17187401ca3f80b066fc225044ed7f1058d
Muhammad  Suleman <muhammad.suleman@ip-192-168-10-3.ec2.internal>
2023-05-10 03:31:27 +0500

    batch jobs

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,6 +1,6 @@
 module "ecr" {
   source                            = "terraform-aws-modules/ecr/aws"
-  version                           = "1.4.0"
+  version                           = "1.6.0"
   repository_name                   = var.name
   repository_read_write_access_arns = var.read_write_access_arns
   repository_lifecycle_policy       = var.lifecycle_policy
