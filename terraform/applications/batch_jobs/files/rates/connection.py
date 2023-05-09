a3315ad1e4f41a9272abe78a3e358590fbe69c8e
Muhammad  Suleman <muhammad.suleman@ip-192-168-10-3.ec2.internal>
2023-05-10 03:18:33 +0500

    batch jobs

--- terraform/applications/batch_jobs/files/rates/connection.py
+++ terraform/applications/batch_jobs/files/rates/connection.py
@@ -1,7 +1,13 @@
 import os
-DB = {
-    "dbname": os.environ["DB_NAME"],
-    "user": os.environ["DB_USER"],
-    "host": os.environ["DB_HOST"],
-    "password" : os.environ["RATES_RW_PASSWORD"]
-}
+from sqlalchemy import create_engine
+
+dbname = os.environ["DB_NAME"]
+user = os.environ["DB_USER"]
+host = os.environ["DB_HOST"]
+password = os.environ["RATES_RW_PASSWORD"]
+
+
+def postgres_connection():
+    db_string = "postgresql://"+user+":"+password+"@"+host+":5432/"+dbname
+    engine = create_engine(db_string)
+    return engine
---
+++ terraform/applications/batch_jobs/files/rates/rates_insertion.py
@@ -0,0 +1,21 @@
+import boto3
+import os
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+import pandas as pd
+from connection import postgres_connection
+
+s3 = boto3.resource('s3')
+
+# bucket = os.environ['bucket']
+# key = os.environ['key']
+def main():
+    df = pd.read_sql_query('select * from company',con=postgres_connection())
+    # s3.Bucket(bucket).download_file(key, '/tmp/file')
+    # reading csv files
+    # df=pd.read_csv('/tmp/file')
+    # Insert to table
+    # df.to_sql("company",con=postgres_connection(),if_exists='append',index=False)
+    print(df)
+
+main()
